20a679e233c2b739215fe8a87eabee8de6398234
victorsabino36 <victorsabino36@gmail.com>
2025-11-15 20:39:45 -0300

    feat: corrige versao do delta

--- airflow_home/dags/daily_lakehouse_pipeline.py
+++ airflow_home/dags/daily_lakehouse_pipeline.py
@@ -18,7 +18,7 @@ CRYPTO_SCRIPT_GCS_PATH = f"gs://{ARTIFACTS_BUCKET}/pipelines/process_crypto_pysp
 STOCKS_SCRIPT_GCS_PATH = f"gs://{ARTIFACTS_BUCKET}/pipelines/ingest_stock_api/ingest_stocks.py"
 
 # Pacotes PySpark necessários no Dataproc Serverless
-PYSPARK_PACKAGES = ["io.delta:delta-spark_2.13:3.2.0"]
+PYSPARK_PACKAGES = ["io.delta:delta-spark_2.13:3.4.0"]
 
 # Caminho do projeto dbt (mount automático do Composer)
 DBT_PROJECT_LOCAL_PATH = "/home/airflow/gcs/dags/dbt/lakehouse_models"
@@ -60,25 +60,24 @@ with DAG(
     # Task 1 — Ingestão de Criptomoedas (PySpark -> Bronze)
     # ----------------------------------------------------------------------
     task_ingest_crypto = DataprocCreateBatchOperator(
-        task_id="ingest_crypto_bronze_dataproc",
-        project_id=PROJECT_ID,
-        region=GCP_REGION,
-        gcp_conn_id="google_cloud_default",
-        batch={
-            "pyspark_batch": {
-                "main_python_file_uri": CRYPTO_SCRIPT_GCS_PATH,
-            },
-            "runtime_config": {
-                "properties": {
-                    "spark.jars.packages": ",".join(PYSPARK_PACKAGES)
-                }
+    task_id="ingest_crypto_bronze_dataproc",
+    project_id=PROJECT_ID,
+    region=GCP_REGION,
+    gcp_conn_id="google_cloud_default",
+    batch={
+        "pyspark_batch": {
+            "main_python_file_uri": CRYPTO_SCRIPT_GCS_PATH,
+        },
+        "runtime_config": {
+            "properties": {
+                "spark.jars.packages": ",".join(PYSPARK_PACKAGES),
+                "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
+                "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog"
             }
         }
-    )
+    }
+        )
 
-    # ----------------------------------------------------------------------
-    # Task 2 — Ingestão de Ações (PySpark -> Bronze)
-    # ----------------------------------------------------------------------
     task_ingest_stocks = DataprocCreateBatchOperator(
         task_id="ingest_stocks_bronze_dataproc",
         project_id=PROJECT_ID,
@@ -91,11 +90,14 @@ with DAG(
             },
             "runtime_config": {
                 "properties": {
-                    "spark.jars.packages": ",".join(PYSPARK_PACKAGES)
+                    "spark.jars.packages": ",".join(PYSPARK_PACKAGES),
+                    "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
+                    "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog"
                 }
             }
         }
     )
+
 
     # ----------------------------------------------------------------------
     # Task 3 — Rodar modelos dbt (Silver + Gold)
